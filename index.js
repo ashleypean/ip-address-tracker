b764c46cc5315419afc6a0f9e3e1777d38d7f03e
Ashley Pean <pean.ashley@gmail.com>
2020-10-11 11:41:21 -0400

    Added error checking for bad inputs and support for domain searches

--- index.js
+++ index.js
@@ -1,7 +1,97 @@
-//Initialize map object
-const myMap = L.map('map', {
+//API key would typically be hidden, but this is client-side only
+const api = 'https://geo.ipify.org/api/v1?apiKey=at_WNBMYWKnfPN9JRxlisF0lUDcPxlvf'
+
+//Initialize the map object
+let myMap = new L.map('map', {
   zoomControl: false, 
-}).setView([51.505, -0.09], 15)
+  attributionControl: false
+})
+
+//Initialize the marker object 
+let marker = new L.marker
+
+//Will hold user's default coordinates base on public IP address 
+let defaultCoordinates = []
+
+//List of domain name extensions to check
+const domainExtension = ['.com', '.tech', '.io', '.dev', '.net', '.gov', '.uk', '.ca', '.me', '.us', '.org', '.co', '.edu', '.int']
+
+//Map automatically moves twice on each page load, set a counter to keep track
+let moveEndCount = 0
+
+//FIND USER'S DEFAULT IP ADDRESS, SET MAP & INFO TO USER'S IP ADDRESS
+window.onload = function() {
+  //Call the API
+  fetchAddress(api)
+  //Create map object and load it on screen
+  .then(info => initializeMap(info))
+  //Update DOM info for div.info
+  .then(info => updatePage(info))
+  //Hold user's default longitude and latitutde base on IP address 
+  .then(info => defaultCoordinates = [info.location.lat, info.location.lng])
+  .catch(error => error.message)
+}
+
+//FETCH NEW IP ADDRESS AFTER SUBMISSION
+async function fetchAddress(url){
+  const res = await fetch(url)
+  //console.log(`Response received`)
+  //console.log(res)
+
+  //Error handler
+  if(!res.ok) {
+    const message = `A ${res.status} error has occured: ${res.statusText}`
+
+    //Print error message on screen, erase when user clicks on search bar again 
+    const error = document.querySelector('.error')
+    error.innerHTML = 'Sorry, that is an invalid input. Please try again.'
+    setTimeout(() => error.innerHTML = '', 4000)
+    //ALTERNATE: Clear error message when user clicks on search bar
+    // document.querySelector('input[type="text"]').addEventListener('click', () => error.innerHTML = '')
+
+    throw new Error (console.log(message))
+  }
+
+  //Convert response to parseable JSON
+  const info = await res.json()
+  //console.log(info)
+  defaultCoordinates = [info.location.lat, info.location.lng]
+  return info
+}
+
+//Update page content with new IP address info
+const updatePage = (info) => {
+  //IP Adress
+  document.querySelector('.ip p').innerHTML = info.ip
+
+  //Location 
+  document.querySelector('.location p').innerHTML = `${info.location.city}, ${info.location.region} ${info.location.postalCode}`
+
+  //Timezone
+  document.querySelector('.timezone p').innerHTML = `UTC ${info.location.timezone}`
+
+  //ISP
+  document.querySelector('.isp p').innerHTML = info.isp
+
+  //Adjust view of map and icon 
+  myMap.setView([info.location.lat, info.location.lng])
+  marker.setLatLng([info.location.lat, info.location.lng])
+
+  //Update placeholder text in search bar
+  document.querySelector('input').placeholder = info.ip
+  console.log('Page update completed')
+
+  //Update global address value 
+  address = info.ip
+
+  return info
+}
+
+//Load the map on the screen
+function initializeMap(info) {
+  //console.log(info)
+  //Set view of map object
+  myMap.setView([info.location.lat, info.location.lng], 15)
 
 //Initiate access to Mapbox map imaging
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -12,37 +102,24 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiYXBlYW4xIiwiYSI6ImNrZnpkam95ajAzb3kyeW1paHNpdjd3cWQifQ.N4KX-ONAnPYxfdhN_iyb2Q', 
 }).addTo(myMap);
 
-
-
-//Create a marker
+//Create a map marker icon 
 const icon = L.icon({
   iconUrl: '/images/icon-location.svg', 
   iconSize: [20, 25],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94], 
+  //iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+  shadowSize: [68, 95],
+  shadowAnchor: [22, 94], 
 })
 
-//Initialize marker object 
-const marker = L.marker([51.5, -0.09], {icon: icon}).addTo(myMap)
+//Initialize marker object and set marker location
+marker = L.marker([info.location.lat, info.location.lng], 
+  {icon: icon, 
+  // draggable: 'true', 
+  // autoPan: true, 
+  riseOffset: 300}).addTo(myMap)
 
-
-
-//API REQUEST LOGIC 
-let address = ''
-//API key would typically be hidden, but this is client-side only
-const api = 'https://geo.ipify.org/api/v1?apiKey=at_WNBMYWKnfPN9JRxlisF0lUDcPxlvf&ipAddress='
-
-let latitude = 0.0
-let longitude = 0.0
-
-//FIND USER'S DEFAULT IP ADDRESS, SET MAP & INFO TO USER'S IP ADDRESS
-window.onload = function findDefaultIp() {
-  //Call the API
-  fetchAddress(api)
-  .then(info => updatePage(info))
-  .catch(error => error.message)
+return info
 }
 
 //Collect form input through an event listener
@@ -51,61 +128,45 @@ document.querySelector('form').addEventListener('submit', (e) => {
 
   //Collect form input from text field
   const input = e.target[0].value
-  console.log(`IP adress is: ${input || 'No input - default public IP Address'}`)
 
-  //Reassign url value to include input
-  address = input
-  const url = api + address
+  let url = ''
+
+  //Check if input is a domain or ip address; update fetch url accordingly
+  hasDomainExtension(input) ? url = `${api}&domain=${input}` : url = `${api}&ipAddress=${input}`
 
   //Call the API
-  fetchAddress(url)
-  .then(info => updatePage(info))
+  fetchAddress(url) 
+  .then(info => updatePage(info)) 
   .catch(error => error.message)
 })
 
-//FETCH NEW IP ADDRESS AFTER SUBMISSION
-async function fetchAddress(url){
-  const res = await fetch(url)
-  console.log(`Response received`)
-  console.log(res)
+function hasDomainExtension(input) {
+  return domainExtension.some(el => input.includes(el))
+}
 
-  //Error handler
-  if(!res.ok) {
-    const message = `A ${res.status} error has occured: ${res.statusText}`
-    throw new Error (console.log(message))
+const backButton = document.querySelector('button.back')
+
+//Display button.back when user moves the map
+myMap.on('moveend', () => {
+  ++moveEndCount
+  //console.log(moveEndCount)
+  if(moveEndCount > 2 && myMap.setView !== defaultCoordinates) {
+    backButton.style.display='block'
   }
+})
 
-  //Convert response to parseable JSON
-  const info = await res.json()
-  console.log(info)
+//EXTRA FEATURE: UPDATE LOCATION WHEN MARKER IS DRAGGED
+// marker.on('click', onDrag)
 
-  return info
-}
+// function onDrag(e) {
+//   console.log('dragged')
+// }
 
-//Update page content when new IP is inputted
-const updatePage = (info) => {
-  console.log('Page update in progress')
-
-  //IP Adress
-  document.querySelector('.ip p').innerHTML = info.ip
-
-  //Location 
-  document.querySelector('.location p').innerHTML = `${info.location.city}, ${info.location.region} ${info.location.postalCode}`
-
-  console.log('Almost done...')
-
-  //Timezone
-  document.querySelector('.timezone p').innerHTML = `UTC ${info.location.timezone}`
-
-  //ISP
-  document.querySelector('.isp p').innerHTML = info.isp
-  console.log('Page has been updated!')
-
-  //Adjust view of map and icon 
-  myMap.setView([info.location.lat, info.location.lng])
-  marker.setLatLng([info.location.lat, info.location.lng])
-  
-}
-
-
-
+//Move map so that marker is in center when back button is clicked
+backButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  myMap.setView(defaultCoordinates)
+  backButton.style.display='none'
+  //Reset end count to 1, browser will count as 1 move when setView method is called
+  moveEndCount = 1
+})
